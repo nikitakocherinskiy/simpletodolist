68ac4a323a834b7a6f096e3219b80da4f1bf62ac
nikitakocherinskiy <nik.koch.67@gmail.com>
2022-08-08 17:19:21 +0300

    init: add todo-list project

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+let todos = [];
+const todosNode = document.querySelector('.js-todos')
+const inputNode = document.querySelector('.js-input')
+const btnNode = document.querySelector('.js-btn');
+
+function addTodo(text){
+	const todo = {
+		text,
+		done: false,
+		id: `${Math.random()}`
+	};
+
+	todos.push(todo)
+}
+
+function deleteTodo(id){
+	todos.forEach(todo => {
+		if (todo.id === id){
+			todo.done = true;
+		}
+	})
+}
+
+function render(){
+	console.log(todos)
+	let html = '';
+
+	todos.forEach(todo => {
+		if (todo.done){
+			return
+		}
+		html += `
+				<div class="title todo">
+					${todo.text}
+					<button data-id="${todo.id}" class="btn">Done</button>
+				</div>`
+		;
+	})
+
+	todosNode.innerHTML = html;
+}
+
+btnNode.addEventListener('click', () => {
+	if (inputNode.value !== '' && inputNode.value !== ' '){
+		const text = inputNode.value;
+		addTodo(text);
+		render();
+	}
+	inputNode.value = '';
+});
+
+todosNode.addEventListener('click', (event) => {
+	if (event.target.tagName !== 'BUTTON') {
+		return;
+	}
+	const id = event.target.dataset.id;
+	deleteTodo(id);
+	render();
+})
+
+render();
